669ba3d5eec25d0674d9d0a7fd84decb1f41c764
ourcompan1 <ourcompany39@gmail.com>
2023-07-03 14:22:43 +0300

    Python moduls
    что это
    как его есть
    как свои создавать
    что такое фреймворки

---
+++ example_module.py
@@ -0,0 +1,7 @@
+hello = 100
+hi = -100
+
+if hello > hi:
+    print('ok\n!!!')
+else:
+    print("not okay")
--- import.py
+++ import.py
@@ -6,15 +6,41 @@
 # print(m.pi)
 
 # from math import pi     # импортировать определенный модуль
+# from math import sqrt as s # rename определнный модуль
 # print(pi)
+# print(s(49))
 
 # from random import *   # import all moduls names
 # print(random())
 
-# and we can do some custom modules , create some file and import moduls from this code file
 
 
+#############
 
+
+# import time
+# time.sleep()
+# print("Hi")
+
+# import datetime as dt
+# print(dt.datetime.now().time().minute) # .нау. какую из дат сегодня . что из под дат сегодня
+
+# # import sys, os, time, math, random, platform
+# import sys  # можно как и в 1 строке все библиотеке , но я считаю лучше по 1 отдельной
+
+# print(platform.system())
+#
+#
+# Create ur own modul and use them
+
+# import example_module as em
+# print(em.hi)
+#
+#
+# Download in terminal come moduls or a lot moduls in one( frameworks )
+
+# import cowsay
+# cowsay.cow('im a cow !  <> nice to meet you !')
 
 
 
